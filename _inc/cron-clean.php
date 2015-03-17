148bf6bfd1265a3cfa64c61580c229d73691bf9c
Konstantin Kiritsenko <konstantink@vissina.com>
2015-03-16 21:22:44 -0700

    Cron File Mods Updated require statment to be more universal. Implemented direct database clean to speed up removal

--- _inc/cron-clean.php
+++ _inc/cron-clean.php
@@ -1,29 +1,43 @@
 <?php
  /**
- * @company SilverTribe
- * @author Aug <mycollegebox@aol.com>
+ * @author Aug Steyer <augsteyer@gmail.com>
  * Created: 3/7/2015 | 17:47
  */
 
-
 global $wpdb;
 
+//just a more complicated include
 if(!isset($wpdb)) {
-	require_once("../../../../wp-load.php");
+	$paths = array(
+		dirname(dirname(dirname(dirname(__FILE__)))) . '/wp-load.php',
+		'../../../../wp-load.php',
+		'../../../wp-load.php',
+		'../../wp-load.php',
+		'../../wp-load.php',
+		'../wp-load.php',
+		'wp-load.php',
+	);
+
+	foreach ($paths as $path) {
+		if (file_exists($path)) {
+			require $path;
+			break;
+		}
+	}
 }
 
+//Remove meta from all posts that have it
 if(isset($_GET['key']) && $_GET['key'] === '4151' && isset($_GET['type'])){
 	if (in_array($_GET['type'], array('daily','weekly','monthly'))){
 		$type = "post_views_count_{$_GET['type']}";
-		 $args = array(
-			'meta_key'         => $type,
-			'post_type'        => 'post',
-			'post_status'      => 'publish',
+		$wpdb->query(
+			$wpdb->prepare(
+				"
+                DELETE FROM $wpdb->postmeta
+		 		WHERE meta_key = %s
+				", $type
+			)
 		);
-		$posts = get_posts( $args );
-		foreach($posts as $post){
-			delete_post_meta($post->ID, $type);
-		}
+		echo "Successfully cleaned up '{$type}' meta tags\n";
 	}
-
 }
